606dee02f562795fb3be763b601646fa93a71ffc
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-10-12 18:04:00 +0200

    carga de datos ok, y verificados mismas columnas con mismos nombres.

---
+++ src/soporte_limpieza.py
@@ -0,0 +1,16 @@
+""""""
+
+# ini imports
+import pandas as pd # type: ignore
+
+pd.set_option('display.max_columns', None) # para poder visualizar todas las columnas de los DataFrames
+
+# fin imports
+
+#cargamos los datos de los ficheros y devolvemos un array de todos los dataframe cargados
+def limpieza_dataframe(df):
+    pass
+
+
+
+
--- src/soporte_load_data.py
+++ src/soporte_load_data.py
@@ -1,4 +1,12 @@
-""""""
+"""
+Este módulo proporciona funciones para cargar, combinar y guardar datos en formato CSV.
+
+Incluye las siguientes funciones:
+- load_all_data(): carga todos los archivos CSV desde un directorio específico y los devuelve como una lista de DataFrames.
+- union_all_data(df_array): combina una lista de DataFrames en un solo DataFrame.
+- save_to_file(df_all_data_union, nombre): guarda un DataFrame combinado en un archivo CSV.
+- check_columns_equal(df_array): verifica si todos los DataFrames tienen las mismas columnas.
+"""
 
 # ini imports
 import pandas as pd # type: ignore
@@ -14,30 +22,64 @@ path_ficheros_salida = '../datos/output/'
 
 #cargamos los datos de los ficheros y devolvemos un array de todos los dataframe cargados
 def load_all_data():
-    df_array = []
-    list_ficheros_clean = []
+    try:
+        df_array = []
+        list_ficheros_clean = []
 
-    list_ficheros = os.listdir("../datos")
+        list_ficheros = os.listdir("../datos")
 
-    for filtrado in list_ficheros:
-        if ".csv" in filtrado:
-            list_ficheros_clean.append(filtrado)
+        for filtrado in list_ficheros:
+            if ".csv" in filtrado:
+                list_ficheros_clean.append(filtrado)
 
-    for fichero in list_ficheros_clean:
-        df_array.append(pd.read_csv(path_ficheros + fichero, sep = ';'))
+        for fichero in list_ficheros_clean:
+            try:
+                df_array.append(pd.read_csv(path_ficheros + fichero, sep = ';'))
+            except:
+                print(f"Error al cargar el fichero {path_ficheros + fichero} en la funcion load_all_data")        
 
-    return df_array
+        return df_array
+    except:
+        print("Error en la funcion load_all_data")
+        return df_array
 
+# hacemos la union de todos los dataframe, se le pasa la lista y el concat lo hace
 def union_all_data(df_array):
+    try:
+        return  pd.concat(df_array, axis=0, ignore_index = True)
+    except:
+        print("Error en la funcion union_all_data")
 
-    return  pd.concat(df_array, axis=0, ignore_index = True)
-
+# guardamos el dataframe con todos los datos a un fichero
 def save_to_file(df_all_data_union, nombre):
+    try:
+        if not os.path.exists(path_ficheros_salida):
+            os.mkdir(path_ficheros_salida)
 
-    if not os.path.exists(path_ficheros_salida):
-        os.mkdir(path_ficheros_salida)
+        df_all_data_union.to_csv(path_ficheros_salida + nombre)
+    except:
+        print("Error en la funcion save_to_file")
 
-    df_all_data_union.to_csv(path_ficheros_salida + nombre)
+# verificamos que todos los ficheros tienen exactamente las mismas columnas
+def check_columns_equal(df_array):
+    try:
+        for df_elment in df_array:
+
+            list_columnas = pd.DataFrame(df_elment).columns
+
+            for df_elment_inside in df_array:
+                list_columnas_inside = pd.DataFrame(df_elment_inside).columns
+
+                if set(list_columnas) != set(list_columnas_inside):
+                    return False
+
+        return True 
+    except:
+        print("Error en la funcion check_columns_equal")      
+
+
+
+
 
 
 
